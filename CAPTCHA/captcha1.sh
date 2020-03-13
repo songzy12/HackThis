c8f6b2d3ba741cef24646fbbf3b4ec8918a4426e
Zhengyang Song <songzy_thu@163.com>
2020-03-14 02:45:33 +0800

    use curl to store session info

--- CAPTCHA/captcha1.sh
+++ CAPTCHA/captcha1.sh
@@ -1,17 +1,20 @@
 # https://defendtheweb.net/extras/playground/captcha/captcha1.php
 
-curl -c cookies.txt https://defendtheweb.net/auth \
--d "username=songzy12" \
--d "password=xxxx"
+set -e
 
-curl -b cookies.txt https://defendtheweb.net/extras/playground/captcha/captcha1.php -o captcha.png
-tesseract captcha.png result
-value=`cat result.txt`
-trim_value=`echo $value | sed 's/ //g' | sed 's/.$//g'`
+curl -v -c cookies.txt https://defendtheweb.net/auth \
+    -d "username=songzy12" \
+    -d "password=xxx"
+
+timestamp=$(date +%F-%H-%M-%S)
+curl -v -b cookies.txt https://defendtheweb.net/extras/playground/captcha/captcha1.php -o data/$timestamp-captcha.png
+tesseract data/$timestamp-captcha.png data/$timestamp-result
+
+value=$(cat data/$timestamp-result.txt)
+trim_value=$(echo $value | sed 's/ //g' | sed 's/.$//g')
+rev_value=$(echo $trim_value | rev)
 echo "trim_value=$trim_value"
-rev_value=`echo $trim_value | rev`
 
-curl -X POST \
--d "answer=$trim_value" \
--b cookies.txt \
-https://defendtheweb.net/playground/captcha1
+curl -v -b cookies.txt \
+    --data-urlencode "answer=$rev_value" \
+    https://defendtheweb.net/playground/captcha1
