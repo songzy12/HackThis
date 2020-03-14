18b58d46d0fc85f12db81606f22c28ee83962f81
songzy <songzy.thu@qq.com>
2020-03-14 21:57:20 +0800

    get lstm.train and spit it into train and test.

--- CAPTCHA/generate_line_box.sh
+++ CAPTCHA/generate_line_box.sh
@@ -5,4 +5,11 @@ for fullfile in data/*.png; do
     filename="${filename%.*}"
     echo $filename
     python3 tesstrain/generate_line_box.py --image=data/$filename.png --txt=data/$filename.txt > data/$filename.box
+    tesseract data/$filename.png data/$filename lstm.train
 done
+
+ls -1 data/*.lstmf | sort -R > data/all-lstmf
+
+holdout_count=1
+head -n  $holdout_count data/all-lstmf > list.eval
+tail -n +$holdout_count data/all-lstmf > list.train
